9c82ac78be1ef4187cd4063c8fdb0f1c88c27f86
Priyanka-BN11 <78422557+Priyanka-BN11@users.noreply.github.com>
2026-03-12 09:23:23 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+void main() => runApp(VoiceToTextApp());
+
+
+class VoiceToTextApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Voice to Text',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: VoiceHomePage(),
+      
+    );
+  }
+}
+
+class VoiceHomePage extends StatefulWidget {
+  @override
+  _VoiceHomePageState createState() => _VoiceHomePageState();
+}
+
+class _VoiceHomePageState extends State<VoiceHomePage> {
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = 'Press the mic button and start speaking';
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  
+  }
+}
+void _listen() async {
+    if (!_isListening) {
+      bool available=await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if(available){
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+          }),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
