b14f7efc1f0aa5a077c3d1b6812c8e92b4392490
Priyanka-BN11 <78422557+Priyanka-BN11@users.noreply.github.com>
2026-03-13 11:57:27 +0100

    working speech to text function

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
+
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 void main() => runApp(VoiceToTextApp());
 
-
 class VoiceToTextApp extends StatelessWidget {
+  const VoiceToTextApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Voice to Text',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       debugShowCheckedModeBanner: false,
       home: VoiceHomePage(),
-      
     );
   }
 }
 
 class VoiceHomePage extends StatefulWidget {
+  const VoiceHomePage({super.key});
+
   @override
   _VoiceHomePageState createState() => _VoiceHomePageState();
 }
@@ -33,16 +32,15 @@ class _VoiceHomePageState extends State<VoiceHomePage> {
   void initState() {
     super.initState();
     _speech = stt.SpeechToText();
-  
   }
-}
-void _listen() async {
+
+  void _listen() async {
     if (!_isListening) {
-      bool available=await _speech.initialize(
+      bool available = await _speech.initialize(
         onStatus: (val) => print('onStatus: $val'),
         onError: (val) => print('onError: $val'),
       );
-      if(available){
+      if (available) {
         setState(() => _isListening = true);
         _speech.listen(
           onResult: (val) => setState(() {
@@ -54,3 +52,28 @@ void _listen() async {
       setState(() => _isListening = false);
       _speech.stop();
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Voice to Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _listen,
+        child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text(
+            _text,
+            style: const TextStyle(fontSize: 24.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
